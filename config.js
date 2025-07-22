352b389ce7d4ebd5e8785cae721f03ef3297d669
👑LORD OBITO 👑 <jnguessan672@gmail.com>
2025-07-22 19:51:47 -0300

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ module.exports = {
     CHATBOT: getConfig("CHATBOT") || "on",
     //chatbot on/off
   
-    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~uBFzzD4Q#DG3cAr6KpOB5dANWCKy7ORPUtwrXvakh9J1I7Hexgig",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~uBFzzD4Q#DGcAr6KpOB5dANWCKy7ORPUtwrXvakh9J1I7Hexgig",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ".",  // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "MEGALODON-MD",  // Bot's display name
     MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
